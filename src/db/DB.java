88b1a5188334a11e45335b0d7014ae952ba228f8
André Fonseca <decobf15@gmail.com>
2024-03-27 15:38:16 -0300

    Department Entity Class completed

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,46 +10,43 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
-	private static Connection conn = null;	
-	
+
+	private static Connection conn = null;
+
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
-	}	
-	
+	}
+
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
 				conn.close();
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-			
+
 		}
 	}
-		
+
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
@@ -59,7 +56,7 @@ public class DB {
 			}
 		}
 	}
-	
+
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
 			try {
